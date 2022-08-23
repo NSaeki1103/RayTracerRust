ae57acabd4962c42dd91dae24a71c0b9229aabe0
NSaeki1103 <94731454+NSaeki1103@users.noreply.github.com>
2022-08-22 20:37:59 -0700

    Give the sphere a gradient

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn hit_sphere
     center : Vec3,
     radius : f32,
     r : &Ray
-) -> bool
+) -> f32
 {
     let oc = r.origin() - center;
     let a = Vec3::dot(&r.direction(), r.direction());
@@ -18,22 +18,25 @@ fn hit_sphere
 
     let discriminant = b * b - 4.0 * a * c;
 
-    discriminant > 0.0
+    if discriminant < 0.0
+    {
+        return -1.0;
+    }
+    else
+    {
+        return (-b-discriminant.sqrt()) / 2.0 * a;
+    }
+
 
 }
 fn color(r: &Ray) -> Vec3
 {
-    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r)
+
+    let t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, r);
+    if t >= 0.0
     {
-        return Vec3::new(1.0, 0.0, 0.0)
-    }
-    if hit_sphere(Vec3::new(0.5, 0.2, -0.9), 0.5, r)
-    {
-        return Vec3::new(0.0, 1.0, 0.0)
-    }
-    if hit_sphere(Vec3::new(-0.5, 0.2, -0.9), 0.5, r)
-    {
-        return Vec3::new(0.0, 1.0, 0.0)
+        let N = Vec3::unit_vector(&(r.point_at_parameter(t)-Vec3::new(0.0, 0.0, -1.0)));
+        return 0.5 * Vec3::new(N.x() + 1.0, N.y() + 1.0, N.z() + 1.0);
     }
     let unit_direction = Vec3::unit_vector(&r.direction());
     let t : f32 = 0.5 * (unit_direction.y() + 1.0);
